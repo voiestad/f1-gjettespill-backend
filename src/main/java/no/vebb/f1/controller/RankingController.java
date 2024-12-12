6600d9bd38c262a1067302cc8d5386f0b4437685
Vebjørn Søreide Øiestad <vebjorn.oiestad@gmail.com>
2024-12-12 20:38:37 +0100

    Separert sjåfør og konstruktør, og lagt til feilmeldinger

--- src/main/java/no/vebb/f1/controller/RankingController.java
+++ src/main/java/no/vebb/f1/controller/RankingController.java
@@ -26,37 +26,76 @@ public class RankingController {
 	}
 
 	@GetMapping()
-	public String guess(Model model) {
+	public String guess(@RequestParam(value = "success", required = false) Boolean success, Model model) {
+		if (success != null) {
+			if (success) {
+				model.addAttribute("successMessage", "Tippingen din ble lagret");
+			} else {
+				model.addAttribute("successMessage",
+						"Tippingen feilet, vennligst prøv igjen eller kontakt administrator");
+			}
+		}
 		return "guessMenu";
 	}
 
-	@GetMapping("/rank")
+	@GetMapping("/drivers")
 	public String rankDrivers(Model model) {
 		String sql = "SELECT name FROM Driver";
 		List<String> drivers = jdbcTemplate.query(sql, (rs, rowNum) -> rs.getString("name"));
 		model.addAttribute("items", drivers);
 		model.addAttribute("title", "Ranger sjåførene");
+		model.addAttribute("type", "drivers");
+		return "ranking";
+	}
+	
+	@PostMapping("/drivers")
+	public String rankDrivers(@RequestParam List<String> rankedItems, Model model) {
+		String sql = "SELECT name FROM Driver";
+		Set<String> drivers = new HashSet<>((jdbcTemplate.query(sql, (rs, rowNum) -> rs.getString("name"))));
+		String error = validateGuessList(rankedItems, drivers);
+		if (error != null) {
+			logger.warn(error);
+			return "redirect:/guess?success=false";
+		}
+		return "redirect:/guess?success=true";
+	}
+	
+	@GetMapping("/constructors")
+	public String rankConstructors(Model model) {
+		String sql = "SELECT name FROM Constructor";
+		List<String> drivers = jdbcTemplate.query(sql, (rs, rowNum) -> rs.getString("name"));
+		model.addAttribute("items", drivers);
+		model.addAttribute("title", "Ranger konstruktørene");
+		model.addAttribute("type", "constructors");
 		return "ranking";
 	}
 
-	@PostMapping("/rank")
-	public String rankDrivers(@RequestParam List<String> rankedItems, Model model) {
-		String sql = "SELECT name FROM Driver";
-		Set<String> driversCheck = new HashSet<>((jdbcTemplate.query(sql, (rs, rowNum) -> rs.getString("name"))));
-		Set<String> guessedDriver = new HashSet<>();
-		for (String driver : rankedItems) {
-			if (!driversCheck.contains(driver)) {
-				throw new IllegalArgumentException("Invalid driver inputted by user");
-			}
-			if (!guessedDriver.add(driver)) {
-				throw new IllegalArgumentException("Duplicate driver inputted by user");
-			}
-			logger.info("Guessed {}", driver);
+	@PostMapping("/constructors")
+	public String rankConstructors(@RequestParam List<String> rankedItems, Model model) {
+		String sql = "SELECT name FROM Constructor";
+		Set<String> contructors = new HashSet<>((jdbcTemplate.query(sql, (rs, rowNum) -> rs.getString("name"))));
+		String error = validateGuessList(rankedItems, contructors);
+		if (error != null) {
+			logger.warn(error);
+			return "redirect:/guess?success=false";
 		}
-		if (driversCheck.size() != guessedDriver.size()) {
-			throw new IllegalArgumentException("Not all drivers guessed");
+		return "redirect:/guess?success=true";
+	}
+
+	private String validateGuessList(List<String> guessed, Set<String> original) {
+		Set<String> guessedSet = new HashSet<>();
+		for (String competitor : guessed) {
+			if (!original.contains(competitor)) {
+				return String.format("%s is not a valid competitor.", competitor);
+			}
+			if (!guessedSet.add(competitor)) {
+				return String.format("Competitor %s is guessed twice in the ranking.", competitor);
+			}
 		}
-		return "redirect:/guess";
+		if (original.size() != guessedSet.size()) {
+			return String.format("Not all competitors are guessed. Expected %d, was %d.", original.size(), guessedSet.size());
+		}
+		return null;
 	}
 
 	@GetMapping("/tenth")
@@ -74,10 +113,11 @@ public class RankingController {
 		String sql = "SELECT name FROM Driver";
 		Set<String> driversCheck = new HashSet<>((jdbcTemplate.query(sql, (rs, rowNum) -> rs.getString("name"))));
 		if (!driversCheck.contains(driver)) {
-			throw new IllegalArgumentException("Invalid driver inputted by user");
+			logger.warn("'{}' invalid tenth driver inputted by user.", driver);
+			return "redirect:/guess?success=false";
 		}
-		logger.info("Guessed {} on tenth", driver);
-		return "redirect:/guess";
+		logger.info("Guessed '{}' on tenth", driver);
+		return "redirect:/guess?success=true";
 	}
 
 	@GetMapping("/winner")
@@ -95,10 +135,11 @@ public class RankingController {
 		String sql = "SELECT name FROM Driver";
 		Set<String> driversCheck = new HashSet<>((jdbcTemplate.query(sql, (rs, rowNum) -> rs.getString("name"))));
 		if (!driversCheck.contains(driver)) {
-			throw new IllegalArgumentException("Invalid driver inputted by user");
+			logger.warn("'{}', invalid winner driver inputted by user.", driver);
+			return "redirect:/guess?success=false";
 		}
-		logger.info("Guessed {} as winner", driver);
-		return "redirect:/guess";
+		logger.info("Guessed '{}' as winner", driver);
+		return "redirect:/guess?success=true";
 	}
 
 	@GetMapping("/flags")
@@ -111,12 +152,12 @@ public class RankingController {
 			@RequestParam int safetyCar, Model model) {
 		Flags flags = new Flags(yellow, red, safetyCar);
 		if (!flags.hasValidValues()) {
-			model.addAttribute("error", "Invalid values");
-			return "redirect:/flags";
+			model.addAttribute("error", "Verdier kan ikke være negative");
+			return "guessFlags";
 		}
-		logger.info("Guessed {} yellow flags, {} red flags and {} safety cars", flags.yellow, flags.red,
+		logger.info("Guessed '{}' yellow flags, '{}' red flags and '{}' safety cars", flags.yellow, flags.red,
 				flags.safetyCar);
-		return "redirect:/guess";
+		return "redirect:/guess?success=true";
 	}
 
 	class Flags {
