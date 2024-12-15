8ae5fff39427edf187a3f91a1dc6997ad361763c
Vebjørn Søreide Øiestad <vebjorn.oiestad@gmail.com>
2024-12-15 22:35:35 +0100

    Justert poengberegning for flagg

--- src/main/java/no/vebb/f1/scoring/DiffPointsMap.java
+++ src/main/java/no/vebb/f1/scoring/DiffPointsMap.java
@@ -8,13 +8,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 public class DiffPointsMap {
 	  
-	private JdbcTemplate jdbcTemplate;
-	private final boolean diffMode;
 	private Map<Integer, Integer> map;
 
 	public DiffPointsMap(String category, JdbcTemplate jdbcTemplate, int year) {
-		this.jdbcTemplate = jdbcTemplate;
-		diffMode = category.equals("FLAGS");
 		final String sql = "SELECT diff, points FROM DiffPointsMap WHERE category = ? and year = ?";
 		List<Map<String, Object>> result = jdbcTemplate.queryForList(sql, category, year);
 		map = new HashMap<>();
@@ -26,22 +22,10 @@ public class DiffPointsMap {
 	}
 
 	public int getPoints(int diff) {
-		if (diffMode) {
-			return flags(diff);
-		}
 		Integer points = map.get(diff);
 		if (points == null) {
 			return 0;
 		}
 		return points;
-	}
-
-	private int flags(int diff) {
-		int maxPoint = map.get(0);
-		// TODO: implement retrieving number of flags and calculating points
-		if (diff == 0) {
-			return maxPoint;
-		}
-		return 0;
 	}
 }
