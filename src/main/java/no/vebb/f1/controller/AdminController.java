5f6587635d88a7a49429b1d4c17999644867c02e
Vebjørn Søreide Øiestad <vebjorn.oiestad@gmail.com>
2025-01-03 19:06:52 +0100

    Laget brukergrensesnitt for å sette frist for tipping av race

--- src/main/java/no/vebb/f1/controller/AdminController.java
+++ src/main/java/no/vebb/f1/controller/AdminController.java
@@ -270,6 +270,10 @@ public class AdminController {
 		calendar.set(Calendar.YEAR, year);
 		calendar.set(Calendar.MONTH, Calendar.JANUARY);
 		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		calendar.set(Calendar.HOUR, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
 		return calendar.toInstant();
 	}
 	
