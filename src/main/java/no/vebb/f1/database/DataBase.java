fc43123e4ee2ecb64c033fd73547bc87fca8a81e
Vebjørn Søreide Øiestad <vebjorn.oiestad@gmail.com>
2025-01-15 18:29:59 +0100

    Fjernet jdbc fra UserService

--- src/main/java/no/vebb/f1/database/DataBase.java
+++ src/main/java/no/vebb/f1/database/DataBase.java
@@ -1,11 +1,14 @@
 package no.vebb.f1.database;
 
 import java.time.Instant;
+import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import no.vebb.f1.user.User;
 import no.vebb.f1.util.NoAvailableRaceException;
 
 @Service
@@ -34,6 +37,27 @@ public class Database {
 		} catch (EmptyResultDataAccessException e) {
 			throw new NoAvailableRaceException("There is no cutoff for the given raceNumber '" + raceNumber + "'");
 		}
+	}
+
+	public boolean isUserAdmin(UUID id) {
+		final String sql = "SELECT COUNT(*) FROM Admin WHERE user_id = ?";
+		return jdbcTemplate.queryForObject(sql, Integer.class, id) > 0;
+	}
+
+	public User getUserFromId(UUID id) throws EmptyResultDataAccessException {
+		final String sql = "SELECT username, google_id FROM User WHERE id = ?";
+		Map<String, Object> sqlRes = jdbcTemplate.queryForMap(sql, id);
+		String username = (String) sqlRes.get("username");
+		String googleId = (String) sqlRes.get("google_id");
+		return new User(googleId, id, username);
+	}
+
+	public User getUserFromGoogleId(String googleId) throws EmptyResultDataAccessException {
+		final String sql = "SELECT username, id FROM User WHERE google_id = ?";
+		Map<String, Object> sqlRes = jdbcTemplate.queryForMap(sql, googleId);
+		String username = (String) sqlRes.get("username");
+		UUID id = UUID.fromString((String) sqlRes.get("id"));
+		return new User(googleId, id, username);
 	}
 
 }
