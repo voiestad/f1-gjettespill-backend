af591d73613b385e8305adc8da2dbe6aa995b474
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-02-23 12:45:03 +0100

    Sikrer at lenker i header er konsistent uansett hvilken side man er på

--- src/main/java/no/vebb/f1/config/WebConfig.java
+++ src/main/java/no/vebb/f1/config/WebConfig.java
@@ -6,16 +6,20 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import no.vebb.f1.components.BreadcrumbInterceptor;
+import no.vebb.f1.components.HeaderInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private BreadcrumbInterceptor breadcrumbInterceptor;
+	@Autowired
+	private HeaderInterceptor headerInterceptor;
 
 	@SuppressWarnings("null")
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(breadcrumbInterceptor);
+		registry.addInterceptor(headerInterceptor);
     }
 }
