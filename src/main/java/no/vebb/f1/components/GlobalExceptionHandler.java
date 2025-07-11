f0a8f4d7c021638751797020770a3e3b2074216f
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-07-11 19:19:50 +0200

    Remove unused parameters in GlobalExceptionHandler

--- src/main/java/no/vebb/f1/components/GlobalExceptionHandler.java
+++ src/main/java/no/vebb/f1/components/GlobalExceptionHandler.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import no.vebb.f1.user.UserService;
@@ -12,7 +12,7 @@ import no.vebb.f1.util.exception.InvalidYearException;
 import no.vebb.f1.util.exception.NoUsernameException;
 import no.vebb.f1.util.exception.NotAdminException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
@@ -23,12 +23,12 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(InvalidYearException.class)
-    public ResponseEntity<?> handleInvalidYear(InvalidYearException e) {
+    public ResponseEntity<?> handleInvalidYear() {
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NotAdminException.class)
-    public ResponseEntity<?> handleNotAdmin(NotAdminException e) {
+    public ResponseEntity<?> handleNotAdmin() {
         userService.loadUser().ifPresentOrElse(
                 user -> logger.warn("User '{}' tried accessing an admin page without the correct access rights",
                         user.id),
@@ -37,7 +37,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(NoUsernameException.class)
-    public ResponseEntity<?> handleNoUsername(NoUsernameException e) {
+    public ResponseEntity<?> handleNoUsername() {
         return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 }
