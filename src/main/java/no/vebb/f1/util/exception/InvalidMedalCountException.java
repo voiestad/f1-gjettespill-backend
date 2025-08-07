4bf49e0490db7b155c27f5ea3798b3ae69949304
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-08-07 19:17:57 +0200

    Add class for a users medals

---
+++ src/main/java/no/vebb/f1/util/exception/InvalidMedalCountException.java
@@ -0,0 +1,7 @@
+package no.vebb.f1.util.exception;
+
+public class InvalidMedalCountException extends RuntimeException {
+    public InvalidMedalCountException(String message) {
+        super(message);
+    }
+}
