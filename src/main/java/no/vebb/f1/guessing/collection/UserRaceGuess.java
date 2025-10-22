1776cd75dbf7eb186883de91dc892b56c86d6786
Vebjørn Øiestad <102312685+voiestad@users.noreply.github.com>
2025-10-22 21:39:21 +0200

    Convert UserRaceGuess to a record

--- src/main/java/no/vebb/f1/guessing/collection/UserRaceGuess.java
+++ src/main/java/no/vebb/f1/guessing/collection/UserRaceGuess.java
@@ -3,18 +3,7 @@ package no.vebb.f1.guessing.collection;
 import no.vebb.f1.competitors.domain.DriverName;
 import no.vebb.f1.results.domain.CompetitorPosition;
 
-public class UserRaceGuess {
-	
-	public final String user;
-	public final DriverName driver;
-	public final CompetitorPosition position;
-
-	public UserRaceGuess(String username, DriverName driver, CompetitorPosition position) {
-		this.user = username;
-		this.driver = driver;
-		this.position = position;
-	}
-
+public record UserRaceGuess(String username, DriverName driver, CompetitorPosition position) {
 	public static UserRaceGuess fromIUserRaceGuess(IUserRaceGuess iUserRaceGuess) {
 		return new UserRaceGuess(iUserRaceGuess.getUsername(), new DriverName(iUserRaceGuess.getDriverName()),
 				iUserRaceGuess.getStartPosition());
